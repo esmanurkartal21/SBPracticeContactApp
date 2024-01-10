181ce70601d08f778336ca60c4d9643e11a4362b
esmanurkartal21 <esmanurkartal21@gmail.com>
2024-01-10 14:57:46 +0300

    first commit

---
+++ src/main/java/com/raincode/contactapi/repo/ContactRepo.java
@@ -0,0 +1,12 @@
+package com.raincode.contactapi.repo;
+
+import com.raincode.contactapi.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact,String> {
+    Optional<Contact> findById(String id);
+}
